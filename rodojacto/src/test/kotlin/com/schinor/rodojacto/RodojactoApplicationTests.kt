700ca92dd87646bd4fbc775e81ec54434e6cbbba
Schinor <marcioschinor@gmail.com>
2026-05-27 11:07:02 -0300

    feat: Estrutura do Back

---
+++ rodojacto/src/test/kotlin/com/schinor/rodojacto/RodojactoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.schinor.rodojacto
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RodojactoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
